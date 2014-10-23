e7b8896178b0e28c4f502751b84857946cdd792c
Lizard <lizard.komodo@gmail.com>
2014-10-23 11:47:57 +0300

    Fix strong_params check

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ActiveRecord::Base
     end
   end
   def user_params
-    params.require(:user).permit(:provider, :uid, :name, :oauth_expires_at)
+    params.require(:user).permit(:provider, :uid, :name, :oauth_token, :oauth_expires_at)
   end
 end
