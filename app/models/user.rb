621129549dc0bed0d51b026a002593c7b37ffea0
Lizard <lizard.komodo@gmail.com>
2014-10-23 19:50:02 +0300

    fix comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ActiveRecord::Base
       user.save!()
     end
   end
-/*  private
-  def user_params
-    params.require(:user).permit(:provider, :uid, :name, :oauth_token, :oauth_expires_at)
-  end
-*/
+#  private
+#  def user_params
+#    params.require(:user).permit(:provider, :uid, :name, :oauth_token, :oauth_expires_at)
+#  end
+
 end
